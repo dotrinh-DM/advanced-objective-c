53802d666ce1dea8281beae81686dd714564a8ef
Do Trinh <contact@dotrinh.com>
2021-09-20 20:32:50 +0900

    convert 3 bytes to int32

--- objective-c/objective-c/ViewController.m
+++ objective-c/objective-c/ViewController.m
@@ -11,8 +11,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-
+    
+    unsigned char data[] = { 0x65, 0x66, 0x67};
+    long ret = 0;
+    ret |= (long)(data[0] & 0x7F) << 25;
+    ret |= (long)(data[1] & 0x7F) << 18;
+    ret |= (long)(data[2] & 0x7F) << 11;
+    NSLog(@"decimal: %ld", ret);
+    
+    //kq su dung 7 bit trong 3 byte nay la: 3 415 947 264
+    //max co the dat dc khi su dung 7 bit trong 3 byte: 4 294 966 272
+    
+    /*
+     co the that long = int va dich chuyen het cac bit ve phia ben phai cho tiet kiem bo nho
+     11111111 11111111 11111111 00000000
+     ↓
+     00000000 11111111 11111111 11111111
+     */
 }
 
 @end
